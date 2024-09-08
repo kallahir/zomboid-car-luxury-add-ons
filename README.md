1288ca1c99347131459882a24726237cd86278b8
Itallo Rossi B. Lucas <itallorossi@Itallos-Laptop.local>
2024-09-08 17:51:25 +0100

    First version of my first Project Zomboid mod.

---
+++ README.md
@@ -0,0 +1,29 @@
+# Car Luxury Add-Ons
+
+## Overview
+
+This is a Zomboid Mod (my first one) that is going to add some "Luxury" features to all vehicles with the intention of making them safer and saving resources.
+
+## Current Add-Ons
+
+In the upcoming sections I'm going to describe the Add-Ons currently available with a brief description of their behavior and the intention behind their creation.
+
+### Auto-Lock Doors
+
+This first Add-On was created, because we often forget to lock doors when we start driving a vehicle and this can expose us to an enemy opening an unlocked door.
+
+This Add-On gets activated when your vehicle speed goes over **25km/h** and doesn't get automatically de-activated. Also I'm not automatically locking the hood or boot.
+
+### Stop/Start
+
+This Add-On was created with the intention of saving petrol, since in most of the game modes this is a very scarce resource.
+
+This Add-On gets activated when your vehicle speed get below **1km/h**. Once your speed gets too low I assume you're stopping the vehicle and stop the engine to save petrol, if you try to drive again after **Stop/Start** was activated your vehicle will get to **running** immediately, avoiding the **starting** process since the engine was previously started.
+
+## Upcoming Add-Ons
+
+The very next Luxury Add-On I would like to create is the **Auto Lights** where I'm going to turn the headlights on everytime the exterior light gets too low.
+
+## Upcoming Improvements
+
+I'm planning to create a custom Menu Tab where I'll allow the customization of the Add-Ons, for example, I would like to allow the selection of which door(s) will be locked by **Auto-Lock Doors** and which speed it gets activated.
---
+++ media/lua/client/CarLuxuyAddOns.lua
@@ -0,0 +1,118 @@
+local MOD_NAME = "Car Luxuy Add-Ons"
+local MOD_AUTHOR = "kallahir"
+local MOD_VERSION = "1.00"
+
+-- Function to log mod details for troubleshooting purposes
+local function info()
+    print("Mod Loaded: " .. MOD_NAME .. " v." .. MOD_VERSION .. " by " .. MOD_AUTHOR)
+end 
+
+Events.OnGameBoot.Add(info)
+
+local data = {
+    vehicle = nil,
+    stopStart = false
+}
+
+-- Function to auto lock all vehicle doors
+local function autoLockDoors()
+    local doorParts = {"DoorFrontLeft", "DoorFrontRight", "DoorRearLeft", "DoorRearRight"}
+    
+    for _, partID in ipairs(doorParts) do
+        local part = data.vehicle:getPartById(partID)
+        if part then
+            print("[autoLockDoors]" .. partID .. " is present on the vehicle.")
+            local lock = part:getDoor()
+            if lock then
+                lock:setLocked(true)
+                print("[autoLockDoors]" .. partID .. " is now locked.")
+            end
+        end
+    end
+end
+
+-- On player update if the player is inside a vehicle we are going to track its speed
+-- and if the speed breaches a certain threshold all the door will be automatically locked 
+local function onPlayerUpdate(player)
+    if player == nil then
+        print("[onPlayerUpdated] Error: player is nil")
+        return
+    end
+
+    if data.vehicle ~= nil then
+        local speed = data.vehicle:getCurrentSpeedKmHour()
+        if speed == nil then
+            print("[onPlayerUpdated] Vehicle speed couldn't be retrieved")
+            return
+        end
+
+        -- If the vehicle speed gets too low we shut-down the vehicle to save petrol 
+        if speed <= 1 and data.vehicle:isEngineRunning() and not data.stopStart then
+            print("[onPlayerUpdated] Stop/Start activated")
+            data.vehicle:engineDoShuttingDown()
+            data.stopStart = true
+        elseif speed > 1 and data.stopStart then
+            print("[onPlayerUpdated] Stop/Start de-activated")
+            data.stopStart = false
+        -- If the vehicle speed gets above 25km/h we are going to automatically lock all doors 
+        elseif speed > 25 then
+            print("[onPlayerUpdated] Vehicle Speed > 25km/h")
+            autoLockDoors()
+        end
+    end
+end
+
+-- On entering vehicle we are going to save it on shared data structure
+-- and register the OnPlayerUpdate event with our custom method
+local function onEnterVehicle(player)
+    if player == nil then
+        print("[onEnterVehicle] Error: player is nil")
+        return
+    end
+
+    print("[onEnterVehicle] Player " .. player:getDisplayName() .. " has entered a vehicle")
+
+    local vehicle = player:getVehicle()
+    if vehicle == nil then
+        print("[onEnterVehicle] Error: vehicle is nil")
+        return
+    end
+
+    print("[onEnterVehicle] Player " .. player:getDisplayName() .. " has entered " .. vehicle:getId())
+
+    data.vehicle = vehicle
+    Events.OnPlayerUpdate.Add(onPlayerUpdate)
+end
+
+-- On leaving vehicle we are going to clear the vehicle form our shared data structure
+-- and de-register the OnPlayerUpdate event with our custom method
+local function onExitVehicle(player)
+    if player == nil then
+        print("[onExitVehicle] Error: player is nil")
+        return
+    end
+
+    print("[onEnterVehicle] Player " .. player:getDisplayName() .. " has left a vehicle")
+
+    Events.OnPlayerUpdate.Remove(onPlayerUpdate)
+    data.vehicle = nil
+    data.stopStart = false
+end
+
+Events.OnEnterVehicle.Add(onEnterVehicle)
+Events.OnExitVehicle.Add(onExitVehicle)
+
+local function onKeyStartPressed(key)
+    -- if key == Keyboard.KEY_W or key == Keyboard.KEY_N then
+    if key == Keyboard.KEY_W then
+        if data.vehicle ~= nil then
+            print("[onKeyStartPressed] Player is within a vehicle")
+            if data.stopStart then
+                print("[onKeyStartPressed] Player started driving the vehicle")
+                data.vehicle:engineDoRunning()
+            end
+        end
+    end
+end
+
+Events.OnKeyStartPressed.Add(onKeyStartPressed)
